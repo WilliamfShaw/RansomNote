23fb158d80a61d8cbb34cf78e6749e0e0e4aff9a
William Shaw <williamfshaw@icloud.com>
2015-08-17 13:36:37 -0400

    updated css for note

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,55 +1,63 @@
 $(function() {
-  $("#create-note").on('click', makeWord)
-  $("#open-note").on('click', appendToPage)
+  $("#create-note").on('click', makeWord);
+  $("#open-note").on('click', appendToPage);
 })
 
 var wordLetters;
 
 var makeWord = function() {
-  console.log("clicked")
+  console.log("clicked");
   var searchTerm = $("#ransom-note").val();
-  var splitWords = searchTerm.split(" ")
+  var splitWords = searchTerm.split(" ");
   wordLetters = splitWords.map(function(word) {
-    return word.split("")
+    return word.split("");
   })
-  var imageArray = []
+  var imageArray = [];
   for (var i = 0; i < wordLetters.length; i++) {
-    var currentWord = wordLetters[i]
+    var currentWord = wordLetters[i];
     for (var j = 0; j < currentWord.length; j++) {
-      getImage(currentWord[j])
+      getImage(currentWord[j]);
     }
   }
-}
+};
 
 var getImage = function(letter) {
   $.ajax({
     url: "/photos/" + letter,
     method: 'get',
     dataType: "json"
-  }).done(reassemble)
-}
+  }).done(reassemble);
+};
 
 var reassemble = function(data) {
   for (var i = 0; i < wordLetters.length; i++) {
-    var currentWord = wordLetters[i]
+    var currentWord = wordLetters[i];
     for (var j = 0; j < currentWord.length; j++) {
-      var index = currentWord.indexOf(data.photo.title._content.toLowerCase())
+      var index = currentWord.indexOf(data.photo.title._content.toLowerCase());
       if (index != -1) {
-        currentWord[index] = ["https://c4.staticflickr.com/", data.photo.farm, "/", data.photo.server, "/", data.photo.id, "_", data.photo.secret, ".jpg"].join('')
+        currentWord[index] = ["https://c4.staticflickr.com/", data.photo.farm, "/", data.photo.server, "/", data.photo.id, "_", data.photo.secret, ".jpg"].join('');
       }
     }
   }
-}
+};
 
 var appendToPage = function() {
   for (var i = 0; i < wordLetters.length; i++) {
     var currentWord = wordLetters[i];
     var wordDiv = $("<div>").addClass("word").attr('id', "word" + i.toString());
-    $("body").append(wordDiv)
+    $("body").append(wordDiv);
     for (var j = 0; j < currentWord.length; j++) {
-      var div = $("#word" + i.toString())
-      var image = $("<img>").attr("src", currentWord[j]).css('height', '150px')
-      $(div).append(image)
+      var div = $("#word" + i.toString());
+      var image = $("<img>").attr("src", currentWord[j]).css('height', getRandomInt(100, 150) + "px");
+      image.css('border-radius', getRandomInt(0, 50) + "%");
+      image.css('width', getRandomInt(100, 150) + "px");
+      image.css('-webkit-transform', 'rotate('+ getRandomInt(0, 7)+'deg)')
+      $(div).append(image);
     }
   }
-}
+};
+
+
+function getRandomInt (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
